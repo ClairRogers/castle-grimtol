0c9d174142ba11ae3803e58ad926d82263f32b61
Clair Rogers <clair-rogers@outlook.com>
2019-04-03 17:18:03 -0600

    initial setup

--- Project/GameService.cs
+++ Project/GameService.cs
@@ -1,10 +1,116 @@
 using System.Collections.Generic;
 using CastleGrimtol.Project.Interfaces;
+using CastleGrimtol.Project.Models;
 
 namespace CastleGrimtol.Project
 {
-    public class GameService : IGameService
+  public class GameService : IGameService
+  {
+    public Room CurrentRoom { get; set; }
+    public Player CurrentPlayer { get; set; }
+    public bool Running { get; set; }
+
+    private void Initialize()
+    {
+      //create all rooms
+      Room obelisk = new Room("The Obelisk", "desc");
+      Room well = new Room("The Well", "desc");
+      Room foxden = new Room("The Fox Den", "desc");
+      Room treehouse = new Room("The Treehouse", "desc");
+      Room caves = new Room("The Caves", "desc");
+      Room gate = new Room("The Gate", "desc");
+
+      //add exits
+      obelisk.AddExit("south", foxden);
+      obelisk.AddExit("north", caves);
+      obelisk.AddExit("west", well);
+      obelisk.AddExit("east", treehouse);
+      foxden.AddExit("north", obelisk);
+      well.AddExit("east", obelisk);
+      treehouse.AddExit("west", obelisk);
+      caves.AddExit("south", obelisk);
+      caves.AddExit("north", gate);
+      gate.AddExit("south", caves);
+
+      //create items
+      Item yellow = new Item("Yellow Key", "desc");
+      Item purple = new Item("Purple Key", "desc");
+      Item red = new Item("Red Key", "desc");
+
+      //add items
+      foxden.AddItem(purple);
+      well.AddItem(yellow);
+      treehouse.AddItem(red);
+
+      CurrentRoom = obelisk;
+      Running = true;
+
+
+    }
+
+    public void Run()
+    {
+      Initialize();
+    }
+
+
+
+
+
+
+    public void GetUserInput()
     {
 
     }
+
+    public void Go(string direction)
+    {
+
+    }
+
+    public void Help()
+    {
+
+    }
+
+    public void Inventory()
+    {
+
+    }
+
+    public void Look()
+    {
+
+    }
+
+    public void Quit()
+    {
+
+    }
+
+    public void Reset()
+    {
+
+    }
+
+    public void Setup()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void TakeItem(string itemName)
+    {
+
+    }
+
+    public void UseItem(string itemName)
+    {
+
+    }
+  }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -3,8 +3,52 @@ using CastleGrimtol.Project.Interfaces;
 
 namespace CastleGrimtol.Project.Models
 {
-    public class Room : IRoom
+  public class Room : IRoom
+  {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Item> Items { get; set; }
+    public Dictionary<string, IRoom> Exits { get; set; }
+
+
+
+    public void AddExit(string direction, IRoom room)
     {
+      Exits.Add(direction, room);
+    }
+
+    public void AddItem(Item item)
+    {
+      Items.Add(item);
+    }
+
+    public IRoom Travel(string direction)
+    {
+      if (Exits.ContainsKey(direction))
+      {
+        return Exits[direction];
+      }
+      System.Console.WriteLine("There's nothing there!");
+      return (IRoom)this;
+    }
+
+
+    public Room(string name, string desc)
+    {
+      Exits = new Dictionary<string, IRoom>();
+      Items = new List<Item>();
+      Name = name;
+      Description = desc;
 
     }
+
+
+    // public enum Direction
+    // {
+    //   north,
+    //   south,
+    //   east,
+    //   west
+    // }
+  }
 }
